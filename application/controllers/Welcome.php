eb0e90f63f6946169334b3d6ab026d4b6f60371b
Kuntal Sarkar <kuntal.sarkar383@gmail.com>
2020-03-30 12:25:48 +0530

    Two cards are placed in homepage

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -5,8 +5,10 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('templates/header');
+		$data['stylesheet']='homepagestyle.css';
+		$this->load->view('templates/header',$data);
 		$this->load->view('templates/navbar');
+		$this->load->view('HomePage');
 		$this->load->view('templates/footer');
 	}
 }
---
+++ application/views/HomePage.php
@@ -0,0 +1,20 @@
+<div class="container">
+	<div class="row">
+		<div class="col-lg-6 col-md-6 col-sm-6" style="padding-right: 20px;padding-left: 20px;">
+			<div class="card">
+			  <div class="card-container">
+			    <h2><b>Create your own poll in just few steps.</b></h2> 
+			    <button class="shadow-button" style="float:right;">Create Now</button>
+			  </div>
+			</div>
+		</div>
+		<div class="col-lg-6 col-md-6 col-sm-6" style="padding-right: 20px;padding-left: 20px;">
+			<div class="card">
+			  <div class="card-container">
+			    <h2><b>Participate in a poll.</b></h2> 
+			    <p>Interior Designer</p> 
+			  </div>
+			</div>
+		</div>
+	</div>
+</div>
