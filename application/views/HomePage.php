5e745107f1103ae9f61720ddbe2c2d79f5716543
Kuntal Sarkar <kuntal.sarkar383@gmail.com>
2020-03-30 12:50:41 +0530

    .htaccess added and homepage done for now

--- application/views/HomePage.php
+++ application/views/HomePage.php
@@ -4,6 +4,7 @@
 			<div class="card">
 			  <div class="card-container">
 			    <h2><b>Create your own poll in just few steps.</b></h2> 
+			    <p class="visible-lg visible-md">&nbsp;</p><p class="">&nbsp;</p>
 			    <button class="shadow-button" style="float:right;">Create Now</button>
 			  </div>
 			</div>
@@ -12,7 +13,49 @@
 			<div class="card">
 			  <div class="card-container">
 			    <h2><b>Participate in a poll.</b></h2> 
-			    <p>Interior Designer</p> 
+			    <p class="visible-lg visible-md">&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+			    <button class="shadow-button" style="float:right;">Vote here</button>
+			  </div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container"><br><br><br>
+	<div class="col-lg-12 col-md-12 col-sm-12"><h2><b>Recent Polls</b></h2><hr></div><br>
+	<div class="row">
+		<div class="col-lg-6 col-md-6 col-sm-6" style="padding-right: 20px;padding-left: 20px;">
+			<div class="card">
+			  <div class="card-container">
+			    <h3><b>Create your own poll in just few steps.</b></h3> 
+			    <p class="visible-lg visible-md">&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+			    <button class="shadow-button" style="float:right;">Create Now</button>
+			  </div>
+			</div>
+		</div>
+		<div class="col-lg-6 col-md-6 col-sm-6" style="padding-right: 20px;padding-left: 20px;">
+			<div class="card">
+			  <div class="card-container">
+			    <h3><b>Create your own poll in just few steps.</b></h3> 
+			    <p class="visible-lg visible-md">&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+			    <button class="shadow-button" style="float:right;">Create Now</button>
+			  </div>
+			</div>
+		</div>
+		<div class="col-lg-6 col-md-6 col-sm-6" style="padding-right: 20px;padding-left: 20px;">
+			<div class="card">
+			  <div class="card-container">
+			    <h3><b>Create your own poll in just few steps.</b></h3> 
+			    <p class="visible-lg visible-md">&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+			    <button class="shadow-button" style="float:right;">Create Now</button>
+			  </div>
+			</div>
+		</div>
+		<div class="col-lg-6 col-md-6 col-sm-6" style="padding-right: 20px;padding-left: 20px;">
+			<div class="card">
+			  <div class="card-container">
+			    <h3><b>Create your own poll in just few steps.</b></h3> 
+			    <p class="visible-lg visible-md">&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+			    <button class="shadow-button" style="float:right;">Create Now</button>
 			  </div>
 			</div>
 		</div>
